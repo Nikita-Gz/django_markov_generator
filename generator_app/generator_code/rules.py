add4cb832739fb95ca3b8f0a3daf0539139c33fa
Nikita-Gz <ngez.work@gmail.com>
2020-07-05 21:45:11 +0300

    improved error messaging

--- generator_app/generator_code/rules.py
+++ generator_app/generator_code/rules.py
@@ -1,4 +1,8 @@
-from .text_preprocessing import normal_preprocessing
-
 MIN_PROMPT_TEXT_LENGTH_REQUIREMENT = 50
 MAX_KEY_LENGTH = 4
+
+error_descriptions = {
+  'bad_input': ('Please enter some long text (at least {} words).'
+                .format(MIN_PROMPT_TEXT_LENGTH_REQUIREMENT)),
+  'bad_file': 'Error reading file. Check if the file is of correct format (raw text)',
+}
--- generator_app/views.py
+++ generator_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.urls import reverse
 
-from .generator_code.rules import *
+from .generator_code import rules
 from .generator_code.generator_functions import create_frequency_table, generate
 from .generator_code.text_preprocessing import normal_preprocessing
 
@@ -10,39 +10,46 @@ def homepage(request: HttpRequest):
   # todo: make it a proper homepage maybe
   return HttpResponseRedirect(reverse('generator_app:text_input'))
 
-def text_input(request: HttpRequest, error=''):
-  err_message = "Please enter some long text (at least {} words).".format(MIN_PROMPT_TEXT_LENGTH_REQUIREMENT)
+def text_input(request: HttpRequest, error_name=''):
+  if error_name != '':
+    err_message = rules.error_descriptions.get(error_name, 'unknown error')
+  else:
+    err_message = ''
 
   context = {
     'entered_text_start': request.session.get('entered_text_start', ''),
     'coma_cbox_status': request.session.get('coma_cbox_status', ''),
     'numbers_cbox_status': request.session.get('numbers_cbox_status', 'checked'),
-    'error_message': (lambda: '' if not error else err_message),
+    'error_message': err_message,
   }
 
   return render(request, 'generator_app/text_input.html', context)
 
-def ask_to_enter_text_again(request: HttpRequest):
-  return HttpResponseRedirect(reverse('generator_app:text_input_err', args=('fuck',)))
+def ask_to_enter_text_again(error_name: str):
+  return HttpResponseRedirect(reverse('generator_app:text_input_err', args=(error_name,)))
 
 def submit_input(request: HttpRequest):
   separate_comas = 'separate_comas' in request.POST.keys()
   replace_number = 'remove_numbers' in request.POST.keys()
 
+  # load text either from file or input
   if 'txt_file' in request.FILES:
-    text = "".join(map(chr, request.FILES['txt_file'].read()))
+    try:
+      text = "".join(map(chr, request.FILES['txt_file'].read()))
+    except Exception as e:
+      return ask_to_enter_text_again('bad_file')
   else:
     text = request.POST['typed_text']
 
   processed = normal_preprocessing(text,
                                   remove_numbers=replace_number,
                                   split_by_punctuation=separate_comas)
-  if len(processed) < MIN_PROMPT_TEXT_LENGTH_REQUIREMENT:
-    return HttpResponseRedirect(reverse('generator_app:text_input_err', args=('fuck',)))
+  if len(processed) < rules.MIN_PROMPT_TEXT_LENGTH_REQUIREMENT:
+    return ask_to_enter_text_again('bad_input')
 
   request.session['input_given'] = True
   request.session['entered_text_start'] = ' '.join(processed[:8]) + str('..')
-  request.session['frequency_table'] = create_frequency_table(processed, MAX_KEY_LENGTH, True)
+  request.session['frequency_table'] = create_frequency_table(processed, rules.MAX_KEY_LENGTH, True)
   request.session['coma_cbox_status'] = (lambda: 'checked' if separate_comas else '')()
   request.session['numbers_cbox_status'] = (lambda: 'checked' if replace_number else '')()
 
@@ -55,13 +62,13 @@ def submit_input(request: HttpRequest):
 
 def generator_page(request: HttpRequest):
   if not request.session.get('input_given', False):
-    return HttpResponseRedirect(reverse('generator_app:text_input_err', args=('fuck',)))
+    return ask_to_enter_text_again('bad_input')
 
   context = {
     'last_prompt': request.session['last_prompt'],
     'generated_text': request.session['generated_text'],
     'stop_reason': request.session['stop_reason'],
-    'key_length': request.session.get('key_length', MAX_KEY_LENGTH // 2),
+    'key_length': request.session.get('key_length', rules.MAX_KEY_LENGTH // 2),
     'text_length': request.session.get('text_length', 50),
     'entered_text_start': request.session['entered_text_start'],
   }
